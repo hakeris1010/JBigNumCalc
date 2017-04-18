e333144f3509b2808e26b7a037f314f8384ee2c3
hakeris1010 <hakeris1010@gmail.com>
2017-04-18 21:51:43 +0300

    Updated GitIgNore and Added GUI Designer Form.

---
+++ src/jbignums/CalcDesigns/JetBrainsGUIDesign.java
@@ -0,0 +1,14 @@
+package jbignums.CalcDesigns;
+
+import javax.swing.*;
+
+/**
+ * Created by Kestutis on 2017-04-18.
+ */
+
+/**
+ *  Basic interface for all Gui Designs
+ */
+public interface JetBrainsGUIDesign {
+    public JPanel getRootContentPanel();
+}
